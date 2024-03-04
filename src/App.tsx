2ee0ff4e1df9a9704ca418b09ce18c1f7700e15b
MD Rayhan Howlader <rhowlader5314@gmail.com>
2024-03-04 16:40:11 +0800

    added increment decrement and increment by amount with using type check

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './index.css';
 import { RootState } from './redux/store';
+import { decrement, increment, incrementByAmount } from './redux/features/counter/counterSlice';
 
 function App() {
   const {count} = useSelector((state: RootState) => state.counter)
@@ -10,11 +11,12 @@ function App() {
     <div>
       <h1>Tech Net</h1>
      <div className='flex justify-center gap-2'>
-     <div className='border-sky-1 '>
-        Increment
+     <div className='border-sky-1 ' onClick={() => dispatch(decrement())}>
+        Decrement
       </div>
       <div>{count}</div>
-      <div className='border-sky-100'>Decrement</div>
+      <div className='border-sky-100' onClick={() => dispatch(increment())}>Increment</div>
+      <div className='border-sky-100' onClick={() => dispatch(incrementByAmount(5))}>Increment by Value</div>
      </div>
 
     </div>
--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -1,4 +1,4 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {PayloadAction, createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
     count: 0,
@@ -9,12 +9,17 @@ const counterSlice = createSlice({
     initialState,
     reducers: {
          increment: (state) => {
-state.count -= 1;
+            state.count += 1;
          },
-         decrement: () => {
-
+         decrement: (state) => {
+            state.count -= 1;
+         },
+         incrementByAmount: (state, action: PayloadAction<number>) => {
+            state.count += action.payload
          }
     }
 })
 
+
+export const {increment, decrement, incrementByAmount} = counterSlice.actions;
 export default counterSlice.reducer;
