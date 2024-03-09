6d6c0f3844e930c6fa67cf2deb240385dccb5f29
MD Rayhan Howlader <rhowlader5314@gmail.com>
2024-03-10 01:20:22 +0800

    I have used redux thunk for handling authentication using email and password. it was has been done. added extra reducer

--- src/redux/features/user/userSlice.ts
+++ src/redux/features/user/userSlice.ts
@@ -1,4 +1,6 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { auth } from "@/lib/firebase";
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 interface IUserState {
   user: {
@@ -7,6 +9,11 @@ interface IUserState {
   isLoading: boolean;
   isError: boolean;
   error: string | null;
+}
+
+interface ICredential {
+  email: string;
+  password: string;
 }
 
 const initialState : IUserState = {
@@ -18,10 +25,27 @@ const initialState : IUserState = {
    error: null 
 }
 
+export const createUser = createAsyncThunk(
+  "user/createUser",
+  async ({email, password} : ICredential) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+
+    return data.user.email;
+  }
+
+);
+
 const userSlice = createSlice({
     name: 'user',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: (builder) => {
+      builder.addCase(createUser.pending, (state) => {
+        state.isLoading = true
+      }).addCase(createUser.fulfilled, (state) => {
+
+      }).addCase(createUser.rejected, (state) => {})
+    }
 })
 
 export default userSlice.reducer;
